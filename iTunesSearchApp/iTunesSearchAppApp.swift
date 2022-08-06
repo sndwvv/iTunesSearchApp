945da3a4cbe87fa38c63ae3b0e90590a9fa9dc56
Songyee Park <songparkhk@gmail.com>
2022-08-06 22:16:28 +0900

    Initial Commit

---
+++ iTunesSearchApp/iTunesSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchAppApp.swift
+//  iTunesSearchApp
+//
+//  Created by Songyee Park on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
